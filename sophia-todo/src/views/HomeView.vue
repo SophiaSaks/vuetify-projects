3b0ac72c1edd9d49551764508c72c016e95f9aeb
SophiaSaks <82449876+SophiaSaks@users.noreply.github.com>
2022-05-18 16:19:30 +0200

    status markers
    status markers for cards in homepage

--- sophia-todo/src/views/HomeView.vue
+++ sophia-todo/src/views/HomeView.vue
@@ -3,24 +3,24 @@
   <h1 class="subheading grey--text">Homepage</h1>
 
   <v-container class="my-5">
-    <v-card flat class="pa-4" outlined>
+    <v-card flat v-for="project in projects" :key="project.title">
       
-        <v-layout row wrap>
+        <v-layout row wrap :class="`pa-3 project ${project.status}`">
           <v-flex xs12 md6>
             <div class="caption grey--text">Project Title</div>
-            <div>Create a new website</div>
+            <div> {{project.title}}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Person</div>
-            <div>Sophia</div>
+            <div> {{project.person}}</div>
           </v-flex>
              <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Due by</div>
-            <div>1 Jan 2022</div>
+            <div>{{project.due}}</div>
           </v-flex>
              <v-flex xs2 sm4 md2>
             <div class="caption grey--text">Status</div>
-            <div>ongoing</div>
+            <div>{{project.status}}</div>
           </v-flex>
 
         </v-layout>
@@ -35,6 +35,30 @@
 <script>
 
   export default {
+    data() {
+      return {
+        projects: [
+          { title: 'Design a new website', person: 'Sophia', due: '18 May 2022', status: 'ongoing', content: 'lorem ipsum'},
+          { title: 'learn vuetify', person: 'Sophia', due: '25 May 2022', status: 'complete', content: 'lorem ipsum'},
+          { title: 'Fix the background for website', person: 'Sophia', due: '17 May 2022', status: 'overdue', content: 'lorem ipsum'},
+        ],
+      }
+    }
 
   }
 </script>
+
+<style>
+
+.project.complete {
+  border-left: 4px solid #66DD8F;
+}
+
+.project.ongoing {
+  border-left: 4px solid #44828F;
+}
+
+.project.overdue {
+  border-left: 4px solid #DD6D66;
+}
+</style>
