a0a87b6d21d530300bdb054f011b208bbcb4b632
SophiaSaks <82449876+SophiaSaks@users.noreply.github.com>
2022-05-18 15:13:56 +0200

    added routes
    added routes to sidebar menu

--- sophia-todo/src/components/Navbar.vue
+++ sophia-todo/src/components/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
     <nav>
-        <v-toolbar flat app>
+        <v-app-bar flat app>
+            <v-btn text class="teal--text" @click="drawer = !drawer">
+            <v-icon>mdi-hamburger</v-icon></v-btn>
             <v-toolbar-title class="text-uppercase teal--text">
                 <span class="font-weight-light">Todo</span>
                 <span class="font-weight-bold">Sophia</span>
@@ -12,12 +14,54 @@
                 <v-icon right>mdi-logout</v-icon>
 
             </v-btn>
-        </v-toolbar>
+        </v-app-bar>
+
+        <v-navigation-drawer app v-model="drawer" class="teal lighten-2">
+             <v-sheet
+        color="teal lighten-2"
+        class="pa-4"
+      >
+        <v-avatar
+          class="mb-4"
+          color="grey darken-1"
+          size="64"
+        ></v-avatar>
+
+        <div class="white--text">Sophia S</div>
+        </v-sheet>
+        <v-divider></v-divider>
+            <v-list>
+                <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                    <v-list-item-icon>
+                    <v-icon class="white--text">{{ link.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                     <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+                    </v-list-item-content>
+
+
+                </v-list-item>
+                
+            </v-list>
+
+        </v-navigation-drawer>
     </nav>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            drawer: false,
+            links: [
+                {icon: 'mdi-home', text: 'Home', route: '/'},
+                {icon: 'mdi-folder', text: 'My Projects', route: '/projects'},
+                {icon: 'mdi-account-group', text: 'Team', route: '/team'},
+
+            ]
+        }
+    }
 
 };
 </script>
